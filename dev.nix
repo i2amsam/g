32bec6f1ed83cb85549d1aa91c2bed482acd7bfc
templateconnectio <templateconnectio@gmail.com>
2024-07-16 20:34:08 +0000

    .

--- dev.nix
+++ dev.nix
@@ -1,11 +1,12 @@
 { pkgs, ... }: {
 
   # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
+  channel = "stable-24.05";  # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.nodejs
+    pkgs.go
   ];
 
   # Sets environment variables in the workspace
--- idx-template.nix
+++ idx-template.nix
@@ -1,19 +1,20 @@
 /*
-rm -rf ./zkit && \
+rm -rf ./test && \
 idx-template \
-  /home/user/genkit-fin/genkit \
-  --output-dir /home/user/genkit-fin/zkit -a '{}'
+  /home/user/g \
+  --output-dir /home/user/g/test -a '{ "language": "go", "tos": "true" }'
 */
-{pkgs, ... }: {  
+{pkgs, language ? "js", tos ? "false", ... }: {  
   packages = [
     pkgs.nodejs
+    pkgs.go
   ];
   bootstrap = ''
     mkdir "$out"
     mkdir "$out"/.idx
     cp ${./dev.nix} "$out"/.idx/dev.nix
     cp ${./README.md} "$out"/README.md
-    env -C "$out" npm init -y
+    ${if language == "go" then "env -C \"$out\" go mod init genkit" else "" }
     env -C "$out" npm i -D genkit
     env -C "$out" npx genkit init --non-interactive -p other -m googleai
     chmod -R u+w "$out"
